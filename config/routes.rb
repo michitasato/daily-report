05606a36140bb97b3ac1bc9271b0252012905d91
michita <nezimakidorizenmai@gmail.com>
2022-01-08 18:19:47 +0900

    fix static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "application#hello"
+  root "records#index"
   post "records/create" => "records#create"
   get "records/new" => "records#new"
   get "records/index" => "records#index"
